f56801b526394c94617f209185f0f79602cc3f7a
master <justinbraun335@gmail.com>
2026-07-18 06:17:50 -0400

    feat: implement linksync.ps1

---
+++ linksync.ps1
@@ -0,0 +1,46 @@
+# ================================================================================
+# =                                   LINKSYNC                                   =
+# ================================================================================
+
+function Sync-DotfileLinks {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Hostname
+    )
+
+    $Output = python "$HOME/parsers/inventory.py" links $Hostname
+
+    foreach ($Line in $Output) {
+        # Ignore $Parts[2], the sudo flag is not needed on windows
+        $Parts = $Line -split ' '
+        $Target = $Parts[0]
+        $Path = $Parts[1]
+
+        # Skip if symlink already exists
+        if ((Test-Path -Path $Path) -and (Get-Item -Path $Path).LinkType -eq 'SymbolicLink') {
+            continue
+        }
+
+        # Ensure dotfiles destination dir exists
+        New-Item -ItemType Directory -Path (Split-Path $Target -Parent) -Force
+
+        # Move existing config into dotfiles
+        if (Test-Path $Path) {
+            Move-Item -Path $Path -Destination $Target
+        }
+
+        # Ensure target parent dir exists
+        New-Item -ItemType Directory -Path (Split-Path $Path -Parent) -Force
+
+        # Create symlink from dotfiles to target
+        New-Item -ItemType SymbolicLink -Path $Path -Target $Target -Force
+    }
+}
+
+function Invoke-Main {
+    $Hostname = $Env:COMPUTERNAME.ToLowerInvariant()
+
+    Sync-DotfileLinks -Hostname $Hostname
+}
+
+Invoke-Main
