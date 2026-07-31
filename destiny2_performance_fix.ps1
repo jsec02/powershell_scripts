bb72237d72235bcd5745c5288fca904c86e0b837
master <justinbraun335@gmail.com>
2026-07-31 06:37:16 -0400

    feat: implement destiny2_performance_fix.ps1

---
+++ destiny2_performance_fix.ps1
@@ -0,0 +1,59 @@
+# ================================================================================
+# =                           DESTINY2_PERFORMANCE_FIX                           =
+# ================================================================================
+
+param(
+    [String]$SteamRoot = 'C:\Program Files (x86)\Steam'
+)
+
+function Assert-SteamInactive {
+    if (Get-Process -Name 'steam' -ErrorAction SilentlyContinue) {
+        Write-Host 'Steam is already running'
+        exit 1
+    }
+}
+
+function Assert-SteamRoot {
+    if (-not (Test-Path -Path $SteamRoot)) {
+        Write-Host "Cannot find the path: $SteamRoot"
+        exit 1
+    }
+}
+
+function Assert-SteamEXE {
+    param(
+        [Parameter(Mandatory)]
+        [String]$SteamEXE
+
+    )
+
+    if (-not (Test-Path -Path $SteamEXE)) {
+        Write-Host "Cannot find the steam executable: $SteamEXE"
+        exit 1
+    }
+}
+
+function Start-Steam  {
+    $SteamEXE = "$SteamRoot\steam.exe"
+
+    Assert-SteamEXE -SteamEXE $SteamEXE
+
+    Start-Process -FilePath $SteamEXE
+}
+
+function Remove-OverlayFiles {
+    # The existence of the steamservice process means that steam has fully started
+    while (-not (Get-Process -Name 'steamservice' -ErrorAction SilentlyContinue)) {
+        Start-Sleep -Seconds 1
+    }
+    Remove-Item -Path "$SteamRoot\*overlay*"
+}
+
+function Invoke-Main {
+    Assert-SteamInactive
+    Assert-SteamRoot
+    Start-Steam
+    Remove-OverlayFiles
+}
+
+Invoke-Main
